94be6f4a8f9465d46aa71cf369ca249fd9699bc2
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-11-19 09:56:00 +0530

    CampusGate

---
+++ src/main/java/com/tka/CampusGateApplication.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusGateApplication.class, args);
+	}
+
+}
